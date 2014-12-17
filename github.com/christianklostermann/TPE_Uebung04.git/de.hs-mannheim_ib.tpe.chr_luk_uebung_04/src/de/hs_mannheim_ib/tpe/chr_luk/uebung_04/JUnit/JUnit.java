fd7e2fa49d076be01ba3d41483818c5de06fc476
christian <christian-klostermann@web.de>
2014-12-17 19:39:50 +0100

    JUnit Test verfolgständigt.

--- github.com/christianklostermann/TPE_Uebung04.git/de.hs-mannheim_ib.tpe.chr_luk_uebung_04/src/de/hs_mannheim_ib/tpe/chr_luk/uebung_04/JUnit/JUnit.java
+++ github.com/christianklostermann/TPE_Uebung04.git/de.hs-mannheim_ib.tpe.chr_luk_uebung_04/src/de/hs_mannheim_ib/tpe/chr_luk/uebung_04/JUnit/JUnit.java
@@ -3,6 +3,8 @@ package de.hs_mannheim_ib.tpe.chr_luk.uebung_04.JUnit;
 import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 import org.junit.Test;
@@ -11,34 +13,42 @@ import de.hs_mannheim_ib.tpe.chr_luk.uebung_04.Crypt.CryptTypen;
 import de.hs_mannheim_ib.tpe.chr_luk.uebung_04.Crypt.Crypter;
 import de.hs_mannheim_ib.tpe.chr_luk.uebung_04.Crypt.CrypterException;
 import de.hs_mannheim_ib.tpe.chr_luk.uebung_04.Crypt.CrypterFactory;
+import de.hs_mannheim_ib.tpe.chr_luk.uebung_04.Crypt.IterableCrypter;
+import de.hs_mannheim_ib.tpe.chr_luk.uebung_04.Crypt.IterableDecrypter;
 
 public class JUnit {
 
 	private String text = "QOZEG]A[UXDKZIZLAB\\NUQIO^^RXYHADV[EFFJ\\\\[\\U_]YDVZABDZT\\V\\SKB@X";
-@Test
+
+	@Test
 	public void test() {
 
-		Crypter cryp = CrypterFactory.createCrypter(CryptTypen.CAESAR, "B");
+		Crypter caesar = CrypterFactory.createCrypter(CryptTypen.CAESAR, "U");
+		Crypter xor = CrypterFactory
+		        .createCrypter(CryptTypen.XOR, "TPEISTCOOL");
 
-		try {
-			System.out.println(cryp.encrypt("HALLO"));
-			List<String> testList = new ArrayList<>();
-			testList.add("BLUB");
-			testList.add("ENIGMA");
-			List<String> cryptoList = cryp.encrypt(testList);
-			System.out.println(cryptoList);
-			System.out.println(cryp.decrypt(cryptoList));
+		List<String> list1 = new ArrayList<>();
+		list1.add("TEST");
+		list1.add("DAS");
 
-		} catch (CrypterException e) {
-			e.printStackTrace();
-		}
+		IterableCrypter iterableCrypter = new IterableCrypter(list1, caesar);
+		IterableDecrypter iterableDecrypter = new IterableDecrypter(iterableCrypter, caesar);
+		
+
+		List<String> list2 = new ArrayList<>();
+		Iterator<String> iterator = iterableDecrypter.iterator();
+
+		while(iterator.hasNext()){
+			 String Text = iterator.next();
+		    	list2.add(Text);		}
+
+		assertEquals(list1, list2);
 	}
 
 	@Test
 	public void testEnsch() throws CrypterException {
 		List<String> testList = new ArrayList<>();
 		testList.add(this.text);
-
 
 		Crypter crypS = CrypterFactory.createCrypter(CryptTypen.SUBSTITUTION,
 		        "MNBVCXYLKJHGFDSAPOIUZTREWQ");
@@ -49,35 +59,43 @@ public class JUnit {
 
 		List<String> list = new ArrayList<>();
 		list.add(text);
-		
+
 		list = crypX.decrypt(list);
 		list = crypR.decrypt(list);
 		list = crypC.decrypt(list);
 		list = crypS.decrypt(list);
 
 		System.out.println(list.toString());
-		
-		String text = "HALLO";	
+
+		String text = "HALLO";
 		assertEquals(crypX.encrypt(text), "A@AXG");
-		assertEquals(crypX.decrypt(crypX.encrypt(text)), text );
-		
-		 text = "HALLO";	
-			assertEquals(crypC.encrypt(text), "TMXXA");
-			assertEquals(crypC.decrypt(crypC.encrypt(text)), text );
-		
-		
+		assertEquals(crypX.decrypt(crypX.encrypt(text)), text);
+
+		text = "HALLO";
+		assertEquals(crypC.encrypt(text), "TMXXA");
+		assertEquals(crypC.decrypt(crypC.encrypt(text)), text);
+
+		text = "HALLO";
+		assertEquals(crypS.encrypt(text), "LMGGS");
+		assertEquals(crypS.decrypt(crypS.encrypt(text)), text);
+
+		text = "HALLO";
+		assertEquals(crypR.encrypt(text), "OLLAH");
+		assertEquals(crypR.decrypt(crypR.encrypt(text)), text);
+
 	}
-    @Test
+
+	@Test
 	public void testCrypXor() {
-	
+
 		Crypter cryp = CrypterFactory.createCrypter(CryptTypen.XOR,
 		        "IAMTHEONEWHOKNOCKS");
-	
+
 		try {
-			String text = "HALLO";	
+			String text = "HALLO";
 			assertEquals(cryp.encrypt(text), "A@AXG");
-			assertEquals(cryp.decrypt(cryp.encrypt(text)), text );
-			
+			assertEquals(cryp.decrypt(cryp.encrypt(text)), text);
+
 		} catch (CrypterException e) {
 			System.out.println("FEHLER beim Cryptographieren!");
 		}
